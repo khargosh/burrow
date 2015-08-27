eed5f0575bd09e90a9df37a5a14178cfab75e947
Yesudeep Mangalapilly <yesudeep@google.com>
2015-08-27 14:03:05 +0530

    Install stuff for go.

---
+++ install-go.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+go get -u github.com/mkouhei/gosh
+go get -u github.com/onsi/ginkgo/ginkgo
+go get -u github.com/onsi/gomega
+go get -u github.com/smartystreets/goconvey
+
+go get -u golang.org/x/mobile
+go get -u golang.org/x/mobile/cmd/gobind
+go get -u golang.org/x/mobile/cmd/gomobile
+
+go get -u github.com/kisielk/errcheck
+go get -u github.com/golang/lint/golint
+go get -u github.com/rogpeppe/godef
+go get -u github.com/nsf/gocode
+go get -u github.com/golang/protobuf/proto
+go get -u github.com/golang/protobuf/protoc-gen-go
+go get -u golang.org/x/review/git-codereview
+go get -u golang.org/x/tools/cmd/benchcmp
+go get -u golang.org/x/tools/cmd/callgraph
+go get -u golang.org/x/tools/cmd/cover
+go get -u golang.org/x/tools/cmd/digraph
+go get -u golang.org/x/tools/cmd/eg
+go get -u golang.org/x/tools/cmd/godex
+go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/gotype
+go get -u golang.org/x/tools/cmd/html2article
+go get -u golang.org/x/tools/cmd/oracle
+go get -u golang.org/x/tools/cmd/present
+go get -u golang.org/x/tools/cmd/ssadump
+go get -u golang.org/x/tools/cmd/stringer
+go get -u golang.org/x/tools/cmd/vet
+go get -u golang.org/x/tour/gotour
